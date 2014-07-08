8898fe0b1d27b95f72baf4810ed2174f27e95ab0
windhunterSB <kongzhao135@163.com>
2014-07-08 14:26:58 +0800

    hehe

--- KocoPro/main.cpp
+++ KocoPro/main.cpp
@@ -4,9 +4,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     MainWindow w;
     w.show();
     return a.exec();
 }
-///just test updata
-///test updata again
+
--- KocoPro/mainwindow.h
+++ KocoPro/mainwindow.h
@@ -20,4 +20,4 @@ private:
 };
 
 #endif // MAINWINDOW_H
-///change sth
+
