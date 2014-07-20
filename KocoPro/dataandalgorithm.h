2b2c8ff67f7eadae61e62b846fe44a0b262b3027
windhunterSB <kongzhao135@163.com>
2014-07-20 16:58:15 +0800

    AddSth

--- KocoPro/dataandalgorithm.h
+++ KocoPro/dataandalgorithm.h
@@ -7,13 +7,25 @@
 
 using namespace std;
 
+struct Segment
+{
+    QPointF Start,End,Center;
+    char Lab;///"L":Line or "C":circle
+    char Arr;///"C":Clock or "U":Unclock
+    Segment(){}
+    Segment(QPointF S,QPointF E,QPointF C,char A):Start(S),End(E),Center(C),Lab('C'),Arr(A){}
+    Segment(QPointF S,QPointF E):Start(S),End(E),Lab('L'){}
+};
+
+
 class DataAndAlgorithm
 {
 public:
     DataAndAlgorithm();
 
 public:
-    vector<QPoint> Path;
+    vector<Segment> Path;
+    vector<QPoint>  DDAPath;///{(+-1,0),(0,+-1),(+-1,+-1)}
     ///http://wenku.baidu.com/view/4f053402bed5b9f3f90f1c24.html
 };
 
